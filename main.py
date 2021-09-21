4045ab1fff85d211e3b078e2ef42bf1308079ad6
77uu <73120837+77uu@users.noreply.github.com>
2021-09-21 10:31:39 +0800

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ SIGN_IN_URL = "https://www.hifini.com/sg_sign.htm"
 
 def main():
     post_data = {"x-requested-with": "XMLHttpRequest"}
-    cookies = {"enwiki_session":r"bbs_sid=1o2dvf9jdef91njnp6loip6pk2; Hm_lvt_4ab5ca5f7f036f4a4747f1836fffe6f2=1627347356,1627347488,1627347805; bbs_token=BCwTQeQThsyfaov4vEF1lVZfQYisNqHvLmo_2BH6YXYtVNlhfjgjXLO4MORSo_2FoEjOfBhAVwFLpftSfBmjzwoYmYDqqySiNPu2; Hm_lpvt_4ab5ca5f7f036f4a4747f1836fffe6f2=1627349789"}
+    cookies = {"enwiki_session":r"bbs_sid=3duptpusvrahu1f5pvak6rlm8b; bbs_token=5P8NWgKQ3xftXY8mj9GEs5eDrb5wyl_2FSSOY3tz6o3N_2FLEGiKfhCuQj2WB_2FmpqpiBEHKXdjsvFD4MymFqcDcERNE5_2FgU_3D; Hm_lvt_4ab5ca5f7f036f4a4747f1836fffe6f2=1631490409,1631522127,1631664465,1631935253; Hm_lpvt_4ab5ca5f7f036f4a4747f1836fffe6f2=1632188930"}
     r1 = requests.post(url=SIGN_IN_URL, data=post_data, cookies=cookies)
     html_text = r1.text
     is_sign = False
